b2dccdf63ba611488745c267259d389f4f99ad4a
KolyasikShutnik <99049907+KolyasikShutnik@users.noreply.github.com>
2022-02-18 00:09:26 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab_3_Ind_16_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            m: Console.WriteLine("Введіть для точки М координату X");
+            float x = float.Parse((Console.ReadLine()));
+            Console.WriteLine("Введіть для точки М координату Y");
+            float y = float.Parse((Console.ReadLine()));
+            if (((x < 0) && (y > 0)) || ((x > 0) && (y < 0))) N = 3;
+            else
+            {
+                if (y - 1 / x > 0) N = 1;
+                else N = 2;
+            }
+            Console.WriteLine('\n' + "Результат:");
+            Console.WriteLine("Точка M(" + x + ";" + y + ")" + " лежить в області N = " + N);
+            Console.WriteLine('\n' + "Для повторного введення натисніть будь-яку клавішу." + '\n' + "Для завершення програми натисніть Enter.");
+            string p = Console.ReadLine();
+            if (p != "") goto m;
+        }
+    }
+}

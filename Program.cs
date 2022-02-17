cc54cba0c43854c1063e1dbf5a986c4c47eb174d
KolyasikShutnik <99049907+KolyasikShutnik@users.noreply.github.com>
2022-02-17 23:52:49 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,24 +1,31 @@
 ﻿using System;
 
-namespace Lab_3_Ind_16_Csharp
+namespace Lab_3_Csharp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int N;
-            m: Console.WriteLine("Введіть для точки М координату X");
+            float f;
+            float max;
+            float min;
+            m: Console.WriteLine("Введіть значення X");
             float x = float.Parse((Console.ReadLine()));
-            Console.WriteLine("Введіть для точки М координату Y");
+            Console.WriteLine("Введіть значення Y");
             float y = float.Parse((Console.ReadLine()));
-            if (((x < 0) && (y > 0)) || ((x > 0) && (y < 0))) N = 3;
+            Console.WriteLine("Введіть значення Z");
+            float z = float.Parse((Console.ReadLine()));
+            max = x * x * x;
+            if (y * y > max) max = y * y;
+            if (x * y > max) max = x * y;
+            min = x;
+            if (y * z < min) min = y * z;
+            if (min * min - y == 0) Console.WriteLine('\n' + "Неможна ділити на нуль");
             else
             {
-                if (y - 1 / x > 0) N = 1;
-                else N = 2;
+                f = (max + x) / (min * min - y);
+                Console.WriteLine('\n' + "Дано: X = " + x + " Y = " + y + " Z = " + z + '\n' + "Результат: F = " + f);
             }
-            Console.WriteLine('\n' + "Результат:");
-            Console.WriteLine("Точка M(" + x + ";" + y + ")" + " лежить в області N = " + N);
             Console.WriteLine('\n' + "Для повторного введення натисніть будь-яку клавішу." + '\n' + "Для завершення програми натисніть Enter.");
             string p = Console.ReadLine();
             if (p != "") goto m;

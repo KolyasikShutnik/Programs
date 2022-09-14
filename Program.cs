fb52f17e9a510b29dbf690bc4590903095b1ce21
KolyasikShutnik <99049907+KolyasikShutnik@users.noreply.github.com>
2022-09-14 15:39:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab_1_OOP_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A Obj = new A();
+            Console.WriteLine($" c = {Obj.C}");
+        }
+    }
+    class A
+        {
+            float a = 2.5f;
+            float b = 4.5f;
+            float c;
+            public float C
+            {
+                get { return (a *= b) - b; } // a = a * b; a - b
+                set { c = value; } // c = a * b - b
+            }
+        }
+}
